0dadcc3f8ac5b47ba2fdd6eb2827ba91616ee68a
karthikrt09 <karthikrt.rt@gmail.com>
2017-06-08 23:17:36 -0700

    StoryBoard done for 2nd view controller

---
+++ Sound Pad/soundRecordViewController.swift
@@ -0,0 +1,28 @@
+//
+//  soundRecordViewController.swift
+//  Sound Pad
+//
+//  Created by Karthik Thirunavukkarasan on 6/8/17.
+//  Copyright © 2017 Karthik Thiru. All rights reserved.
+//
+
+import UIKit
+
+class soundRecordViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var soundName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recordButtonTapped(_ sender: Any) {
+    }
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    @IBAction func AddTapped(_ sender: Any) {
+    }
+}
