872925e10468b3cb285906c0a9f43c7877ad1f6a
karthikrt09 <karthikrt.rt@gmail.com>
2017-06-09 00:05:50 -0700

    created audio session

--- Sound Pad/soundRecordViewController.swift
+++ Sound Pad/soundRecordViewController.swift
@@ -7,22 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class soundRecordViewController: UIViewController {
-
+    
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var soundName: UITextField!
     
+    var audioRecorder : AVAudioRecorder?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupRecorder()
+        
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func recordButtonTapped(_ sender: Any) {
+    
+    func setupRecorder(){
+        
+        do{
+            //create an audio session
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            
+            //create the url fro the audio file
+            
+           
+            
+            //create setting for the audio recorder
+            
+            //create audio recorder object
+            
+            
+            audioRecorder = AVAudioRecorder(url: <#T##URL#>, settings: <#T##[String : Any]#>)
+        }catch{}
     }
-    @IBAction func playTapped(_ sender: Any) {
-    }
-    @IBAction func AddTapped(_ sender: Any) {
-    }
+    
+        @IBAction func recordButtonTapped(_ sender: Any) {
+        }
+        @IBAction func playTapped(_ sender: Any) {
+        }
+        @IBAction func AddTapped(_ sender: Any) {
+        }
 }
